818c7b7ca9103d1b7b5fb82a1d9fbc0bac894a84
khalidzhang <khalidzhang@tencent.com>
2021-04-01 20:26:18 +0800

    add

--- src/language/hello/src/hello_sub.rs
+++ src/language/hello/src/hello_sub.rs
@@ -1,13 +1,16 @@
-
-
 pub mod hello_kk {
     pub mod hello_pub {
         use ::function_name::named;
+
         #[named]
         pub fn hello() {
-            println!("hello from: func: [{} fn] mod_path:  [{}]", function_name!(),module_path!())
+            println!("hello from: func: [{} fn] mod_path:  [{}]", function_name!(), module_path!())
         }
-        fn hello_private(){}
+
+        #[named]
+        fn hello_private() {
+            println!("hello from: func: [{} fn] mod_path:  [{}]", function_name!(), module_path!())
+        }
     }
 
     mod hello_private {}
