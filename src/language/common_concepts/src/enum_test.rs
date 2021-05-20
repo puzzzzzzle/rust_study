7cf8d398cc05557f265a099899579692725f9320
khalidzhang <khalidzhang@tencent.com>
2021-05-20 19:33:17 +0800

    add enum test

--- src/language/common_concepts/src/enum_test.rs
+++ src/language/common_concepts/src/enum_test.rs
@@ -1,9 +1,12 @@
+use std::fmt;
+
 mod row_enum {
     #[derive(Debug)]
     enum IpAddrKind {
         V4,
         V6,
     }
+
     #[derive(Debug)]
     struct IpAddr {
         kind: IpAddrKind,
@@ -25,14 +28,61 @@ mod row_enum {
 }
 
 mod enum_with_str {
+    use std::fmt;
+    use std::fmt::Formatter;
+
+    #[derive(Debug)]
+    struct UnknownIpAddr {
+        name: String,
+        msg: String,
+        value: i32,
+    }
+
+    impl fmt::Display for UnknownIpAddr {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(f, "{},{},{}", self.name, self.msg, self.value)
+        }
+    }
+
+    #[derive(Debug)]
     enum IpAddr {
         V4(String),
         V6(String),
+        UNKNOWN(UnknownIpAddr),
     }
+
+    impl fmt::Display for IpAddr {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            match self {
+                IpAddr::V4(s) => write!(f, "ip4 :{}", s),
+                IpAddr::V6(s) => write!(f, "ip6 :{}", s),
+                IpAddr::UNKNOWN(s) => write!(f, "UNKNOWN :{}", s),
+            }
+        }
+    }
+
     #[test]
-    fn emun_test() {
+    fn enum_test() {
         let home = IpAddr::V4(String::from("127.0.0.1"));
+        println!("{:?}", home);
 
         let loopback = IpAddr::V6(String::from("::1"));
+        println!("{:?}", loopback);
+        let unknown = IpAddr::UNKNOWN(UnknownIpAddr {
+            name: String::from("khalid"),
+            msg: String::from("tao"),
+            value: 42,
+        });
+        println!("{:#?}", unknown);
+        println!("{}", unknown);
+        let some_value = Some(3);
+        let res = if let Some(3) = some_value{
+            println!("three");
+            "aha"
+        }else {
+            "fail"
+        };
+        println!("{}",res)
+
     }
 }
--- src/language/common_concepts/src/main.rs
+++ src/language/common_concepts/src/main.rs
@@ -3,5 +3,6 @@ mod func_test;
 mod struct_test;
 mod test_ownership;
 mod variable_test;
+mod std_colloctions_test;
 
 fn main() {}
