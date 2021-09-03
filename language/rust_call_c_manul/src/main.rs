0dfa738fe9a25a039a067c29bfeeddfee7eb4127
khalidzhang <khalidzhang@tencent.com>
2021-09-03 10:58:03 +0800

    add call c time use

--- language/rust_call_c_manul/src/main.rs
+++ language/rust_call_c_manul/src/main.rs
@@ -1,7 +1,14 @@
 use rust_call_c_manul::{c_zero_safe, get_rs_value_safe};
+use stopwatch::Stopwatch;
 
 fn main() {
+    let mut sw = Stopwatch::start_new();
     println!("Hello, world!");
+    println!("--   time use : {:?}", sw.elapsed());
+    sw.restart();
     println!("call  c {}", c_zero_safe());
+    println!("--   time use : {:?}", sw.elapsed());
+    sw.restart();
     println!("c call call  rs {}", get_rs_value_safe());
+    println!("--   time use : {:?}", sw.elapsed());
 }
