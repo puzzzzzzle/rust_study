9cda566afb2cce8936fa8c80cec7e46dafd1b1ab
khalidzhang <khalidzhang@tencent.com>
2021-09-23 20:41:15 +0800

    add

---
+++ language/common_concepts/tests/co_test.rs
@@ -0,0 +1,26 @@
+use futures::executor::block_on;
+use std::thread::sleep;
+use std::time::Duration;
+
+async fn hello() -> String {
+    String::from("hello co")
+}
+async fn hello_1() -> String {
+    String::from("hello_1 co")
+}
+async fn hello_1_2() -> String {
+    let s1 = hello().await;
+    let s2 = hello_1().await;
+    String::from(format!("hello_1_2 co {} {}", s1, s2))
+}
+async fn hello_latter() -> String {
+    sleep(Duration::from_secs(1));
+    String::from("hello_latter co")
+}
+#[test]
+fn test_1() -> () {
+    let future = hello();
+    let ret = block_on(future);
+    println!("{}", ret);
+    block_on(hello_1_2());
+}
