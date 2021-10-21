1747e3590f5d32c0d7799dbb2b29e13a657a0986
khalidzhang <khalidzhang@tencent.com>
2021-10-21 20:52:53 +0800

    fix co test

--- language/common_concepts/tests/co_test.rs
+++ language/common_concepts/tests/co_test.rs
@@ -11,7 +11,7 @@ async fn hello_1() -> String {
 async fn hello_1_2() -> String {
     let s1 = hello().await;
     let s2 = hello_1().await;
-    String::from(format!("hello_1_2 co {} {}", s1, s2))
+    String::from(format!("hello_1_2 co : ({} {})", s1, s2))
 }
 async fn hello_latter() -> String {
     sleep(Duration::from_secs(1));
@@ -22,5 +22,8 @@ fn test_1() -> () {
     let future = hello();
     let ret = block_on(future);
     println!("{}", ret);
-    block_on(hello_1_2());
+    let ret = block_on(hello_1_2());
+    println!("{}", ret);
+    let ret = block_on(hello_latter());
+    println!("{}", ret);
 }
