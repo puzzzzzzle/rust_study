33d483c597e832a3378e5d1db1563fdd464d9a85
khalidzhang <khalidzhang@tencent.com>
2021-11-25 10:23:27 +0800

    add comment

--- common/benches/log_bench.rs
+++ common/benches/log_bench.rs
@@ -1,19 +1,19 @@
+// 开始test功能
 #![feature(test)]
+// 注意: 目前只有nightly版本才能使用bench
 
-#[cfg(test)]
-mod tests {
-    extern crate test;
-    use test::Bencher;
+// 内部crate需要extern后才能使用
+extern crate test;
+use test::Bencher;
 
-    use common;
-    use log::*;
+use common;
+use log::*;
 
-    #[bench]
-    fn bench_log(b: &mut Bencher) {
-        common::init_env().unwrap();
-        info!("start bench log");
-        b.iter(|| {
-            info!("one log ");
-        });
-    }
+#[bench]
+fn bench_log(b: &mut Bencher) {
+    common::init_env().unwrap();
+    info!("start bench log");
+    b.iter(|| {
+        info!("one log ");
+    });
 }
