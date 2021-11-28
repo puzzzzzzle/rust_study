70dbf5ff80e39ac9cb7aeb21a8ba48a99249ef85
puzzzzzzle <2359173906@qq.com>
2021-11-29 00:12:07 +0800

    fix bugs

--- language/rust_call_c_manul/src/c_wrapper.c
+++ language/rust_call_c_manul/src/c_wrapper.c
@@ -1,23 +1,9 @@
 //
 // Created by 23591 on 2021/9/3.
 //
-#include <stdio.h>
-#include <sys/time.h>
-#include <unistd.h>
 
-int test() {
+int c_zero() { return 0; }
 
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-
-  printf("second: %ld\n", tv.tv_sec);                                 // 秒
-  printf("millisecond: %ld\n", tv.tv_sec * 1000 + tv.tv_usec / 1000); // 毫秒
-  printf("microsecond: %ld\n", tv.tv_sec * 1000000 + tv.tv_usec);     // 徽秒
-
-  return 0;
-}
-
-int c_zero() { return test(); }
 int c_one_t() { return 0; }
 
 extern int rs_zero();
