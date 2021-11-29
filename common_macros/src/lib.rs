7f52c58be426792f9f0dd074a3acbf18f0097ca0
puzzzzzzle <2359173906@qq.com>
2021-11-29 15:06:16 +0800

    add comment

--- common_macros/src/lib.rs
+++ common_macros/src/lib.rs
@@ -1,3 +1,7 @@
+//! this file is used for holding macros defined by me.
+//! proc_macro_attribute and it's func can only appear in lib.rs at the root of crate,
+//! so ues this file to hole all the procedural macros
+
 mod trace_var;
 use proc_macro::TokenStream;
 
