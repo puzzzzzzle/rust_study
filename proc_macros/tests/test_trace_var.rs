bdbada87da52314ee833ac40d94c1bb3882d1c84
khalidzhang <khalidzhang@tencent.com>
2021-12-07 12:46:26 +0800

    change comment

--- proc_macros/tests/test_trace_var.rs
+++ proc_macros/tests/test_trace_var.rs
@@ -1,5 +1,11 @@
-// use crate::trace_var;
+//! test trace var proc mac
+//! 测试 trace var 宏
+
 use proc_macros::trace_var;
+
+
+/// 会在执行时, 对任何p n 的赋值操作, 打印状态
+///
 #[test]
 #[trace_var(p, n)]
 fn factorial() {
