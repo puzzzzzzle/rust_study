85f6423acf2146815745e032324e3a3a7c4b7840
puzzzzzzle <2359173906@qq.com>
2021-12-15 02:00:32 +0800

    add

--- proc_macros/src/lib.rs
+++ proc_macros/src/lib.rs
@@ -2,9 +2,16 @@
 //! proc_macro_attribute and it's func can only appear in lib.rs at the root of crate,
 //! so ues this file to hole all the procedural macros
 
+mod struct_name;
 mod trace_var;
+
 use proc_macro::TokenStream;
 
+/// 过程宏 - 熟悉宏
+/// Attribute to print the value of the given variables each time they are
+/// reassigned.
+/// 对指定的变量的任意改变操作, 打印日志
+///
 #[proc_macro_attribute]
 pub fn trace_var(args: TokenStream, input: TokenStream) -> TokenStream {
     trace_var::trace_var(args, input)
---
+++ proc_macros/src/struct_name.rs
@@ -0,0 +1,17 @@
+//! 对一个struct添加get_name实现
+#[proc_macro_derive(Trait)]
+pub fn derive_trait(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+
+    let expanded = quote! {
+        impl Trait for #name {
+            fn print(&self) -> usize {
+                println!("{}","hello from #name")
+           }
+        }
+    };
+
+    proc_macro::TokenStream::from(expanded)
+}
