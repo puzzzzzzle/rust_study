a31da4b0fb1f159a00d686b6aa628647831dddbe
puzzzzzzle <2359173906@qq.com>
2024-01-02 20:27:26 +0800

    'update'

--- proc_macros/examples/trace_var.rs
+++ proc_macros/examples/trace_var.rs
@@ -4,8 +4,8 @@ use proc_macros::*;
 fn factorial(mut n: u64) -> u64 {
     let mut p = 1;
     while n > 1 {
-        p *= n;
-        n -= 1;
+        p = p * n;
+        n = n - 1;
     }
     p
 }
