25496235be91a5c763f0717eaadb59dac8573773
puzzzzzzle <2359173906@qq.com>
2024-05-30 06:46:04 +0000

    update

--- rust_language/examples/static_test.rs
+++ rust_language/examples/static_test.rs
@@ -1,10 +1,11 @@
-use std::ops::Deref;
+use std::cell::{OnceCell, RefCell};
+use std::os::linux::raw::stat;
+use std::rc::Rc;
 use std::sync::Mutex;
-use lazy_static::lazy::Lazy;
 use lazy_static::lazy_static;
 
 #[derive(Default, Debug)]
-struct StaticData
+pub struct StaticData
 {
     i1: i32,
     d1: f64,
@@ -13,7 +14,7 @@ struct StaticData
 
 pub static mut GLB_DATA: Option<&'static StaticData> = None;
 
-pub fn unsafe_save_gbl_data(data: & StaticData)
+pub fn unsafe_save_gbl_data(data: &StaticData)
 {
     unsafe {
         let data_ptr = data as *const StaticData as *mut StaticData;
@@ -22,18 +23,32 @@ pub fn unsafe_save_gbl_data(data: & StaticData)
     }
 }
 
+thread_local! {
+    pub static GLB_DATA_REF_CELL: RefCell<Option<&'static StaticData>> = RefCell::new(None);
+}
+pub fn unsafe_save_gbl_data_ref_cell(data: &StaticData) {
+    unsafe {
+        let data_ptr = data as *const StaticData as *mut StaticData;
+        let boxed_data = Box::from_raw(data_ptr);
+        let static_data: &'static StaticData = Box::leak(boxed_data);
+        GLB_DATA_REF_CELL.replace(Some(static_data));
+    }
+}
 
 fn main() {
-    let mut data = StaticData {
+    let data = StaticData {
         i1: 42,
         d1: 3.14,
         s1: String::from("Hello, world!"),
     };
 
-    unsafe_save_gbl_data(&mut data);
+    unsafe_save_gbl_data(&data);
     unsafe {
         if let Some(gbl_data) = &GLB_DATA {
             println!("{:?}", gbl_data);
         }
     }
+    unsafe_save_gbl_data_ref_cell(&data);
+    println!("new 2 {:?}", GLB_DATA_REF_CELL.take().unwrap());
+
 }
