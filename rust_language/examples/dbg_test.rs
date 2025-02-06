578fad4e84e7e0710141144deb70d009a9c331ae
puzzzzzzle <2359173906@qq.com>
2025-02-06 15:54:24 +0800

    update

--- rust_language/examples/dbg_test.rs
+++ rust_language/examples/dbg_test.rs
@@ -2,7 +2,7 @@ use rand::Rng;
 
 fn main() {
     let a = dbg!({
-        let mut a = rand::thread_rng().gen_range(1, 101);
+        let mut a = rand::rng().random_range(1.. 101);
         a += 1000;
         a - 100
     });
