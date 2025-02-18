8e7b565dd2cf83780769011b85ac1761f0e77016
puzzzzzzle <2359173906@qq.com>
2025-02-18 19:51:55 +0800

    update

--- rust_language/examples/log_test.rs
+++ rust_language/examples/log_test.rs
@@ -1,20 +1,22 @@
+use anyhow::{Result, anyhow};
 use common;
 use log::*;
 use std::env;
-use anyhow::{Result, anyhow};
+use std::path::PathBuf;
 
-fn main() -> Result<(), > {
+fn main() -> Result<()> {
     common::init_env()?;
     info!("hello info");
     info!(
         "cwd {}",
         env::current_dir().unwrap().as_os_str().to_str().unwrap()
     );
-    let curr_dir = env::current_dir()?
+    let curr_dir = env::current_dir()
+        .unwrap_or(PathBuf::from("get cwd fail"))
         .as_os_str()
         .to_str()
         .ok_or_else(|| anyhow!("cannot convert to str"))?
         .to_string();
-    info!("cwd {curr_dir}" );
+    info!("cwd {curr_dir}");
     Ok(())
 }
