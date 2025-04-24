5bb468d3b82bd2056ba38aed279e3387bbb9c501
puzzzzzzle <2359173906@qq.com>
2025-04-24 12:31:50 +0800

    pass both rs and py tests

--- rust_with_python/py_astar/tests/test_astar.rs
+++ rust_with_python/py_astar/tests/test_astar.rs
@@ -15,5 +15,20 @@ fn test_astar_cls() {
     let goal = Pos(6, 2);
     let result = astar.astar(start, goal);
     let result = result.expect("No path found");
-    println!("path is {:}", Board::path_to_str(&result))
+    println!("path is {:}", Board::path_to_str(&result));
+    assert_eq!(result, vec![
+        Pos(0, 1),
+        Pos(0, 0),
+        Pos(1, 0),
+        Pos(2, 0),
+        Pos(2, 1),
+        Pos(2, 2),
+        Pos(2, 3),
+        Pos(3, 3),
+        Pos(4, 3),
+        Pos(4, 2),
+        Pos(4, 1),
+        Pos(5, 1),
+        Pos(6, 1),
+        Pos(6, 2)])
 }
