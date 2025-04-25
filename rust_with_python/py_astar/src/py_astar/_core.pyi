4fa1dcc4c04aa4de01dd6c175565e2e854cedf18
puzzzzzzle <2359173906@qq.com>
2025-04-25 18:55:41 +0800

    update

--- rust_with_python/py_astar/src/py_astar/_core.pyi
+++ rust_with_python/py_astar/src/py_astar/_core.pyi
@@ -2,76 +2,78 @@
 # module py_astar._core
 # from /data/srcs/Repos/rust_study/rust_with_python/py_astar/src/py_astar/_core.abi3.so
 # by generator 1.147
-""" astar wrapper for python """
+"""astar wrapper for python"""
+
 # no imports
 
 # functions
 
-def version(*args, **kwargs): # real signature unknown
+def version(*args, **kwargs):  # real signature unknown
     pass
 
 # classes
 
 class AStar(object):
-    """ astar impl """
-    def py_allow_diagonal(self, *args, **kwargs): # real signature unknown
+    """astar impl"""
+
+    def py_allow_diagonal(self, *args, **kwargs):  # real signature unknown
         pass
 
-    def py_astar(self, *args, **kwargs): # real signature unknown
+    def py_astar(self, *args, **kwargs):  # real signature unknown
         pass
 
-    def py_set_allow_diagonal(self, *args, **kwargs): # real signature unknown
+    def py_set_allow_diagonal(self, *args, **kwargs):  # real signature unknown
         pass
 
-    def __init__(self, *args, **kwargs): # real signature unknown
+    def __init__(self, *args, **kwargs):  # real signature unknown
         pass
 
-    @staticmethod # known case of __new__
-    def __new__(*args, **kwargs): # real signature unknown
-        """ Create and return a new object.  See help(type) for accurate signature. """
+    @staticmethod  # known case of __new__
+    def __new__(*args, **kwargs):  # real signature unknown
+        """Create and return a new object.  See help(type) for accurate signature."""
         pass
-
 
 class PathFindError(object):
     # no doc
-    def __init__(self, *args, **kwargs): # real signature unknown
+    def __init__(self, *args, **kwargs):  # real signature unknown
         pass
 
-    @staticmethod # known case of __new__
-    def __new__(*args, **kwargs): # real signature unknown
-        """ Create and return a new object.  See help(type) for accurate signature. """
+    @staticmethod  # known case of __new__
+    def __new__(*args, **kwargs):  # real signature unknown
+        """Create and return a new object.  See help(type) for accurate signature."""
         pass
 
-    details = property(lambda self: object(), lambda self, v: None, lambda self: None)  # default
-
-
+    details = property(
+        lambda self: object(), lambda self, v: None, lambda self: None
+    )  # default
 
 class Pos(object):
     # no doc
-    def __init__(self, *args, **kwargs): # real signature unknown
+    def __init__(self, *args, **kwargs):  # real signature unknown
         pass
 
-    @staticmethod # known case of __new__
-    def __new__(*args, **kwargs): # real signature unknown
-        """ Create and return a new object.  See help(type) for accurate signature. """
+    @staticmethod  # known case of __new__
+    def __new__(*args, **kwargs):  # real signature unknown
+        """Create and return a new object.  See help(type) for accurate signature."""
         pass
 
-    x = property(lambda self: object(), lambda self, v: None, lambda self: None)  # default
+    x = property(
+        lambda self: object(), lambda self, v: None, lambda self: None
+    )  # default
 
-    y = property(lambda self: object(), lambda self, v: None, lambda self: None)  # default
-
-
+    y = property(
+        lambda self: object(), lambda self, v: None, lambda self: None
+    )  # default
 
 # variables with complex values
 
 __all__ = [
-    'version',
-    'AStar',
-    'PathFindError',
-    'Pos',
+    "version",
+    "AStar",
+    "PathFindError",
+    "Pos",
 ]
 
-__loader__ = None # (!) real value is '<_frozen_importlib_external.ExtensionFileLoader object at 0x7fe94c6e5510>'
+__loader__ = None  # (!) real value is '<_frozen_importlib_external.ExtensionFileLoader object at 0x7fe94c6e5510>'
 
-__spec__ = None # (!) real value is "ModuleSpec(name='py_astar._core', loader=<_frozen_importlib_external.ExtensionFileLoader object at 0x7fe94c6e5510>, origin='/data/srcs/Repos/rust_study/rust_with_python/py_astar/src/py_astar/_core.abi3.so')"
-
+__spec__ = None  # (!) real value is "ModuleSpec(name='py_astar._core', loader=<_frozen_importlib_external.ExtensionFileLoader object at 0x7fe94c6e5510>, origin='/data/srcs/Repos/rust_study/rust_with_python/py_astar/src/py_astar/_core.abi3.so')"
--- rust_with_python/py_astar/tests/test.py
+++ rust_with_python/py_astar/tests/test.py
@@ -5,23 +5,18 @@
 from py_astar._core import version, AStar, Pos
 import pytest
 
+
 @pytest.fixture
 def sample_board1():
     """测试用地图数据1"""
-    return [
-        "21397X2",
-        "1X19452",
-        "62251X1",
-        "1612179",
-        "1348512",
-        "61453X1",
-        "7861243"
-    ]
+    return ["21397X2", "1X19452", "62251X1", "1612179", "1348512", "61453X1", "7861243"]
+
 
 @pytest.fixture
 def sample_board2():
     """测试用地图数据2（含障碍）"""
-    return ["1"*3+"X"+"1"*6] * 10
+    return ["1" * 3 + "X" + "1" * 6] * 10
+
 
 class TestAStar:
     def test_astar_path_found(self, sample_board1):
@@ -32,9 +27,20 @@ class TestAStar:
         path = [(pos.x, pos.y) for pos in result]
         print(path)
         assert path == [
-            (0, 1), (0, 0), (1, 0), (2, 0), (2, 1),
-            (2, 2), (2, 3), (3, 3), (4, 3), (4, 2),
-            (4, 1), (5, 1), (6, 1), (6, 2)
+            (0, 1),
+            (0, 0),
+            (1, 0),
+            (2, 0),
+            (2, 1),
+            (2, 2),
+            (2, 3),
+            (3, 3),
+            (4, 3),
+            (4, 2),
+            (4, 1),
+            (5, 1),
+            (6, 1),
+            (6, 2),
         ]
 
     def test_astar_simple_path(self, sample_board2):
@@ -51,7 +57,7 @@ class TestAStar:
         astar = AStar(sample_board2)
         with pytest.raises(RuntimeError) as excinfo:
             astar.py_astar(Pos(0, 1), Pos(7, 8))
-        assert str(excinfo.value) == 'Path not found'
+        assert str(excinfo.value) == "Path not found"
 
     def test_astar_doc(self):
         """测试文档字符串"""
