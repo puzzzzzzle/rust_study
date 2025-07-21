a8536bda0a994a7189c002e06c5473ea0f36d700
puzzzzzzle <2359173906@qq.com>
2025-07-21 17:27:12 +0800

    update

---
+++ declarative_macro/examples/macro_use_new.rs
@@ -0,0 +1,6 @@
+// 新的使用方式下, 已经和函数差不多了
+use declarative_macro::{say_hello};
+
+fn main() {
+    say_hello!();
+}
---
+++ declarative_macro/examples/macro_use_old.rs
@@ -0,0 +1,7 @@
+// 老的导入宏的方式
+#[macro_use]
+extern crate declarative_macro;
+
+fn main() {
+    say_hello!();
+}
